f73f1e1a6ad716c4ced781269b172f584c6330b5
Andrew_gzz <andres.rafael.gs@gmail.com>
2026-02-18 09:49:27 -0600

    Remove Experiments component and associated assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './components/landing-page/landing-page';
-import { Experiments } from './components/experiments/experiments';
 
 export const routes: Routes = [
     {
         path: '', component: LandingPage
-    },
-    {
-        path: 'Experiments', component: Experiments
     }
 ];
